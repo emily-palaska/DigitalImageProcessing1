d1e8af8fc19bfe619571231b8368cbc7e46eb802
Emily Palaska <103355365+emily-palaska@users.noreply.github.com>
2024-04-09 21:41:39 +0300

    Add files via upload

---
+++ global_hist_eq.py
@@ -0,0 +1,43 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_equalization_transform_of_img(img_array): 
+    # Get image dimensions
+    n1, n2 = img_array.shape
+    # Define unique values number based on uint8 dtype
+    L = 256
+    
+    # Turn array to 1d for ease of use
+    img_flat = img_array.flatten()
+    
+    # Find the probability of each value happening
+    _, counts = np.unique(img_flat, return_counts=True)
+    p = counts / (n1 * n2)
+    
+    # Find the u vector as the cumulative sum
+    u = np.cumsum(p)
+    
+    equalization_transform = np.round((u - u[0]) * (L - 1) / (1 - u[0]))
+    return equalization_transform
+
+def perform_global_hist_equalization(img_array):
+    T = get_equalization_transform_of_img(img_array)
+    equalized_img = T[img_array]
+    return equalized_img.astype(int)
+    
+# Load the image
+img = Image.open("input_img.png").convert('L')
+# Convert the image to a NumPy uint8 array
+img_array = np.array(img).astype(np.uint8)
+img1 = Image.fromarray(img_array)
+# Perform equalization
+equalized_img = perform_global_hist_equalization(img_array)
+
+img2 = Image.fromarray(equalized_img)
+
+img1.save("img1.png")
+img2.save("img2.png")
+
+#plt.hist(np.concatenate(equalized_img))
+#plt.show()
